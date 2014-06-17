e3a5cd2a01befef370bac6c3850913be0c67439f
cclauss <cclauss@bluewin.ch>
2014-06-18 01:27:02 +0200

    Update highscores.py to move to is_high_score()
    Is_high_score() returns True or False to the caller to let them know if the score is really a new high score.  Often the caller wants to notify the user of their achievement and congratulate them.  We also now check the initial score to ensure that it is numeric.

--- highscores.py
+++ highscores.py
@@ -1,36 +1,41 @@
 import pickle
-import os
-global high_scores, file_name
 
-def setup_scores():
-	global high_scores, file_name
-	high_scores = {}
-	file_name = 'highscores.pkl'
+global high_scores
 
-	if os.path.isfile(file_name):
-		with open(file_name,"r") as h:
-				high_scores = pickle.load(h)
-	else:
-		pass
+file_name = 'highscores.pkl'
 
-def save_score(name, score):
-	if name in high_scores:
-		if score > high_scores[name]:
-			high_scores[name] = score
-    else:
+def load_scores():
+    try:
+        with open(file_name, 'rb') as in_file:
+            return pickle.load(in_file)
+    except IOError:
+        return {}
+
+def save_scores():
+    with open(file_name, 'wb') as out_file:
+        pickle.dump(high_scores, out_file)
+
+def is_high_score(name, score):
+    global high_scores
+    try:
+        curr_high_score = high_scores.get(name, score-1)
+    except TypeError:
+        raise TypeError('The score arguement must be a number.')
+    is_new_high_score = score > curr_high_score
+    if is_new_high_score:
         high_scores[name] = score
-
-	with open(file_name,"w") as out:
-		pickle.dump(high_scores, out)
+        save_scores()
+    return is_new_high_score
 
 def print_scores():
-	score_line = "{{name:>{col_width}}} | {{score}}".format(col_width=(80-3)//2)
-	for name, score in high_scores.items():
-		print(score_line.format(name=name, score=score))
+    score_line = '{{name:>{col_width}}} | {{score}}'.format(col_width=(80-3)//2)
+    for name, score in high_scores.items():
+        print(score_line.format(name=name, score=score))
 
-setup_scores()  # this is ran on import and on run
+high_scores = load_scores()  # this is run on import and on run
 
 if __name__ == '__main__':  # this is run on run only
     # raw_input used only for testing, normally use variables or values instead
-    save_score(raw_input('Name: ').title(), int(raw_input('Score: ')))
+    if is_high_score(raw_input('Name: ').title(), int(raw_input('Score: '))):
+        print('Congratulations on your new high score!')
     print_scores()
